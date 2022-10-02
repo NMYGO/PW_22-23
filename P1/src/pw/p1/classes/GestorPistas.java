a8929bf640c32aeee5d43d24152ee637b58f8789
i92tacod <71820298+i92tacod@users.noreply.github.com>
2022-10-02 20:09:17 +0200

    Add files via upload

--- P1/src/pw/p1/classes/GestorPistas.java
+++ P1/src/pw/p1/classes/GestorPistas.java
@@ -26,7 +26,7 @@ public class GestorPistas{
 				return false;
 			}
 		}
-		Pista newKart = new Kart(id, tipo, estado);
+		Kart newKarts = new Kart(id, tipo, estado);
 		arrayKarts.add(newKarts);
 		return true;
 	}
@@ -40,22 +40,23 @@ public class GestorPistas{
 
 		for (int i = 0;i< arrayPistas.size() ; i++) {
 			pista_ = arrayPistas.get(i);
+			ArrayList<Kart> list_ = pista_.getLkart();
 
-			if (!pista_.getEstado() && pista_.lkart.size() != pista_getMaxkarts(){
+			if (!pista_.isEstado() && list_.size() != pista_.getMaxkarts()){
 				maxkarts_ = pista_.getMaxkarts();
 
 				for (int j = 0; j< arrayKarts.size() && cont <= maxkarts_; j++) {
 					kart_ = arrayKarts.get(j);
 
-					if (kart_.estado == "DISPONIBLE") {
+					if (kart_.getEstado() == Kart.Estado.DISPONIBLE) {
 
-						if (kart_.isTipo() == true && (pista_.getDificultad() == "FAMILIAR" || pista_.getDificultad() == "INFANTIL")) {
+						if (kart_.isTipo() == true && (pista_.getDificultad() == Pista.Dificultad.FAMILIAR || pista_.getDificultad() == Pista.Dificultad.INFANTIL)) {
 							listakarts_ = pista_.getLkart();
 							listakarts_.add(kart_);
 							pista_.setLkart(listakarts_);
 							cont++;
 						}
-						if (kart_.isTipo() == false && (pista_.getDificultad() == "FAMILIAR" || pista_.getDificultad() == "ADULTO")) {
+						if (kart_.isTipo() == false && (pista_.getDificultad() == Pista.Dificultad.FAMILIAR || pista_.getDificultad() == Pista.Dificultad.ADULTO)) {
 							listakarts_ = pista_.getLkart();
 							listakarts_.add(kart_);
 							pista_.setLkart(listakarts_);
@@ -69,13 +70,13 @@ public class GestorPistas{
 
 	public void listaPistasMantenimiento(){
 		for (int i = 0;i< arrayPistas.size() ; i++) {
-			if (arrayPistas.get(i).isEstado) {
-				System.out.println(arrayPistas.get(i).getNombre);
+			if (arrayPistas.get(i).isEstado()) {
+				System.out.println(arrayPistas.get(i).getNombre());
 			}
 		}
 	}
 
-	public ArrayList<Pistas> pistasLibres(Integer kartnum, Dificultad tipo){
+	public ArrayList<Pista> pistasLibres(Integer kartnum, Dificultad tipo){
 		
 		ArrayList<Pista> arraypistas;
 
