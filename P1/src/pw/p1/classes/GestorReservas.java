afbd1e5852b94e2ff4e8a6d096ed72a56bb8109e
MORE <i02mocaf@uco.es>
2022-10-04 00:04:55 +0200

    DESARROLLO GESTOR RESERVAS

--- P1/src/pw/p1/classes/GestorReservas.java
+++ P1/src/pw/p1/classes/GestorReservas.java
@@ -1,5 +1,41 @@
 package pw.p1.classes;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class GestorReservas {
 
+	/* Atributos */
+	
+	/**
+	 * Constructor por defecto
+	 **/	
+	public GestorReservas(){}
+	
+	/**
+	 * Realiza una reserva individual
+	 **/
+	
+	public boolean ReservaIndividualInfantil(Usuario usuario, Pista pista, int nparticipantes) {
+		if((usuario.getNacimiento()).isBefore(LocalDate.now().minusYears(18))) {
+			if((pista.getDificultad() == Pista.Dificultad.INFANTIL) && (pista.isEstado() == false)) {
+				Pista kpista = new Pista();
+				ArrayList<Kart> dkart = kpista.consultarKartsDisponibles();
+				int infantilkart = 0;
+				for (int i = 0;i < dkart.size() ; i++) {
+					if ((dkart.get(i)).isTipo() == true) {
+						infantilkart ++;
+					}
+				}
+				if(infantilkart <= nparticipantes) {
+					if(usuario.getInscripcion().isBefore(LocalDate.now().minusYears(2))) {
+						
+					}
+				}
+			}
+		}
+		
+		return true;
+	}
+	
 }
