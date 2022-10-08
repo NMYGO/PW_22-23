159a7b9e5acd82401c7cdf52aa3bb218769115a1
david <david@DavidPC>
2022-10-08 19:14:49 +0200

    Reservas Individuales Infantil y Adulta

--- P1/src/pw/p1/classes/GestorReservas.java
+++ P1/src/pw/p1/classes/GestorReservas.java
@@ -99,119 +99,175 @@ public class GestorReservas {
 	
 	
 	
-	/*public boolean ReservaIndividualAdulto(Usuario usuario, Pista pista, int nParticipantes, Scanner scan_, GestorPistas GestorPistas_) {
-		if ((usuario.getNacimiento()).isBefore(LocalDate.now().minusYears(18))) {
-			ArrayList<Kart> dkart = pista.consultarKartsDisponibles(GestorPistas_.arrayKarts);
-			int karts = 0;
-			for (int i = 0;i < dkart.size() ; i++) {
-				if ((dkart.get(i)).isTipo() == false) {
-					karts++;
+	public boolean ReservaIndividualAdulto(GestorReservas GestorReservas_, GestorPistas GestorPistas_, GestorUsuarios GestorUsuarios_, int nParticipantes, Scanner scan_) {
+		System.out.println("Introduzca su correo de usuario");
+		String correo = scan_.nextLine();
+		System.out.println("");
+		for (int i = 0; i < GestorUsuarios_.arrayUsuarios.size(); i++) {
+			if (GestorUsuarios_.arrayUsuarios.get(i).getCorreo().equals(correo)) {
+				if ((GestorUsuarios_.arrayUsuarios.get(i).getNacimiento()).isBefore(LocalDate.now().minusYears(18))) {
+					System.out.println("Introduzca el numero de participantes (adultos)");
+					int participantes= Integer.parseInt(scan_.nextLine());
+					System.out.println("");
+					ArrayList<Pista> lpista = GestorPistas_.pistasLibres(scan_, participantes, Dificultad.ADULTO);			
+					if(lpista.size() > 0) {
+						System.out.println("Pistas libres:");
+						for (int j = 0; j < lpista.size(); j++) {
+							System.out.println(lpista.get(j).toString());
+						}
+						System.out.println("");
+						System.out.println("Escoja una pista mediante su nombre");
+						String pista = scan_.nextLine();
+						System.out.println("");
+						boolean valido = false;
+						while(!valido) {
+							for (int j = 0; j < lpista.size(); j++) {
+								if(!lpista.get(j).getNombre().equals(pista)) {
+									System.out.println("Escoja una pista mediante su nombre");
+									pista = scan_.nextLine();
+									System.out.println("");
+								}else {
+									valido = true;
+								}
+							}
+						}
+						float precio = 0;
+						int descuento = 0;
+						System.out.println("Introduzca la duración de la reserva (60/90/120 minutos)");
+							int duracion = Integer.parseInt(scan_.nextLine());
+						System.out.println("Introduzca la fecha de la reserva (yyyy-mm-dd)");
+							LocalDate fecha = LocalDate.parse(scan_.nextLine());
+							switch(duracion) {
+							case 60:
+								precio = 20;
+								break;
+							case 90:
+								precio = 30;
+								break;
+							case 120:
+								precio = 40;
+								break;
+							}	
+						if(GestorUsuarios_.arrayUsuarios.get(i).getInscripcion().isBefore(LocalDate.now().minusYears(2))) {
+							descuento = 10;
+						}
+						
+						RIndividualCreador individualCreador = new RIndividualCreador();
+						RAdulto newReserva = individualCreador.creaRAdu(GestorUsuarios_.arrayUsuarios.get(i).getNombre(), fecha, duracion, pista, precio, descuento, participantes);
+						arrayReservaIndividual.add(newReserva);
+						System.out.println("Reserva creada con exito");
+						System.out.println("-------------------------------------");
+						System.out.println("");
+					} else {
+						System.out.println("Error. No existen pistas libres");
+						System.out.println("");
+						return false;
+					}
+				} else {
+					System.out.println("Error. Las reservas se realizan solo por adultos");
+					System.out.println("");
+					return false;
 				}
-			}
-			if(karts>=nParticipantes) {
-				RIndividualCreador individualCreador = new RIndividualCreador();
-				RAdulto reserva = individualCreador.creaRAdu();
-				System.out.println("Introduzca la duración de la reserva (30/60/90 minutos)");
-				int tiempo = Integer.parseInt(scan_.nextLine());
-				System.out.println("Introduzca la fecha de la reserva");
-				LocalDate fecha = LocalDate.parse(scan_.nextLine());
-				reserva.setDur(tiempo);
-				reserva.setFecha(fecha);
-				reserva.setParticipantes(nParticipantes);
-				reserva.setUsuario(usuario.getNombre());
-				reserva.setPista(pista.getNombre());
-				switch(tiempo) {
-				case 30:
-					reserva.setPrecio(20);
-					break;
-				case 60:
-					reserva.setPrecio(30);
-					break;
-				case 90:
-					reserva.setPrecio(40);
-					break;
-				}
-				if(usuario.getInscripcion().isBefore(LocalDate.now().minusYears(2))) {
-					reserva.setDesc(10);
-				}
-				else reserva.setDesc(0);
-			}
-			else {
-				System.out.println("No hay suficientes karts en esa pista para los participantes.\n");
-				return false;
 			}
 		}
-		else {
-			System.out.println("No se puede hacer una reserva para adultos con un usuario menor de edad.\n");
-			return false;
-		}
-		
 		return true;
-	}*/
+	}
 	
 	
 	
-	/*public boolean ReservaIndividualFamiliar(Usuario usuario, Pista pista, Scanner scan_, GestorPistas GestorPistas_) {
-		if ((usuario.getNacimiento()).isBefore(LocalDate.now().minusYears(18))) {
-			ArrayList<Kart> dkart = pista.consultarKartsDisponibles(GestorPistas_.arrayKarts);
-			int karts = 0;
-			int nParticipantes;
-			System.out.println("Introduzca numero de adultos");
-			int adultos = Integer.parseInt(scan_.nextLine());
-			System.out.println("Introduzca numero de niños");
-			int ninos = Integer.parseInt(scan_.nextLine());
-			for (int i = 0;i < adultos ; i++) {
-				if ((dkart.get(i)).isTipo() == false) {
-					karts++;
+	public boolean ReservaIndividualFamiliar(GestorReservas GestorReservas_, GestorPistas GestorPistas_, GestorUsuarios GestorUsuarios_, int nParticipantes, Scanner scan_) {
+		System.out.println("Introduzca su correo de usuario");
+		String correo = scan_.nextLine();
+		System.out.println("");
+		for (int i = 0; i < GestorUsuarios_.arrayUsuarios.size(); i++) {
+			if (GestorUsuarios_.arrayUsuarios.get(i).getCorreo().equals(correo)) {
+				if ((GestorUsuarios_.arrayUsuarios.get(i).getNacimiento()).isBefore(LocalDate.now().minusYears(18))) {
+					System.out.println("Introduzca el numero de participantes (niños)");
+						int participantesInfantiles= Integer.parseInt(scan_.nextLine());
+						System.out.println("");
+						System.out.println("Introduzca el numero de participantes (adultos)");
+						int participantesAdultos= Integer.parseInt(scan_.nextLine());
+						System.out.println("");
+						int participantes = participantesAdultos + participantesInfantiles;
+					ArrayList<Pista> lpista = GestorPistas_.pistasLibres(scan_, participantes, Dificultad.FAMILIAR);			
+					for (int j = 0; j < lpista.size(); j++) {
+						ArrayList<Kart> lkart = new ArrayList<Kart>();
+						lkart = lpista.get(j).getLkart();
+						int nKartInfantil = 0;
+						int nKartAdulto = 0;
+						for (int k = 0; k < lkart.size(); k++) {
+							if (lkart.get(k).isTipo()) {
+								nKartInfantil += 1;
+							}else {
+								nKartAdulto += 1;
+							}
+						}
+						if (participantesInfantiles > nKartInfantil || participantesAdultos > nKartAdulto) {
+							lpista.remove(j);
+						}
+					}
+					if(lpista.size() > 0) {
+						System.out.println("Pistas libres:");
+						for (int j = 0; j < lpista.size(); j++) {
+							System.out.println(lpista.get(j).toString());
+						}
+						System.out.println("");
+						System.out.println("Escoja una pista mediante su nombre");
+						String pista = scan_.nextLine();
+						System.out.println("");
+						boolean valido = false;
+						while(!valido) {
+							for (int j = 0; j < lpista.size(); j++) {
+								if(!lpista.get(j).getNombre().equals(pista)) {
+									System.out.println("Escoja una pista mediante su nombre");
+									pista = scan_.nextLine();
+									System.out.println("");
+								}else {
+									valido = true;
+								}
+							}
+						}
+						float precio = 0;
+						int descuento = 0;
+						System.out.println("Introduzca la duración de la reserva (60/90/120 minutos)");
+							int duracion = Integer.parseInt(scan_.nextLine());
+						System.out.println("Introduzca la fecha de la reserva (yyyy-mm-dd)");
+							LocalDate fecha = LocalDate.parse(scan_.nextLine());
+							switch(duracion) {
+							case 60:
+								precio = 20;
+								break;
+							case 90:
+								precio = 30;
+								break;
+							case 120:
+								precio = 40;
+								break;
+							}	
+						if(GestorUsuarios_.arrayUsuarios.get(i).getInscripcion().isBefore(LocalDate.now().minusYears(2))) {
+							descuento = 10;
+						}
+						
+						RIndividualCreador individualCreador = new RIndividualCreador();
+						RFamiliar newReserva = individualCreador.creaRFam(GestorUsuarios_.arrayUsuarios.get(i).getNombre(), fecha, duracion, pista, precio, descuento, participantesInfantiles, participantesAdultos);
+						arrayReservaIndividual.add(newReserva);
+						System.out.println("Reserva creada con exito");
+						System.out.println("-------------------------------------");
+						System.out.println("");
+					} else {
+						System.out.println("Error. No existen pistas libres");
+						System.out.println("");
+						return false;
+					}
+				} else {
+					System.out.println("Error. Las reservas se realizan solo por adultos");
+					System.out.println("");
+					return false;
 				}
-			}
-			for (int i = 0; i< ninos; i++) {
-				if ((dkart.get(i)).isTipo() == true) {
-					karts++;
-				}
-			}
-			nParticipantes=adultos+ninos;
-			if(dkart.size()>=nParticipantes) {
-				RIndividualCreador individualCreador = new RIndividualCreador();
-				RFamiliar reserva = individualCreador.creaRFam();
-				System.out.println("Introduzca la duración de la reserva (30/60/90 minutos)");
-				int tiempo = Integer.parseInt(scan_.nextLine());
-				System.out.println("Introduzca la fecha de la reserva");
-				LocalDate fecha = LocalDate.parse(scan_.nextLine());
-				reserva.setDur(tiempo);
-				reserva.setFecha(fecha);
-				reserva.setadultos(adultos);
-				reserva.setninos(ninos);
-				reserva.setUsuario(usuario.getNombre());
-				reserva.setPista(pista.getNombre());
-				switch(tiempo) {
-				case 30:
-					reserva.setPrecio(20);
-					break;
-				case 60:
-					reserva.setPrecio(30);
-					break;
-				case 90:
-					reserva.setPrecio(40);
-					break;
-				}
-				if(usuario.getInscripcion().isBefore(LocalDate.now().minusYears(2))) {
-					reserva.setDesc(10);
-				}
-				else reserva.setDesc(0);
-			}
-			else {
-				System.out.println("No hay suficientes karts en esa pista para los participantes.\n");
-				return false;
 			}
 		}
-		else {
-			System.out.println("No se puede hacer una reserva para adultos con un usuario menor de edad.\n");
-			return false;
-		}
-		
 		return true;
-	}*/
+	}
 	
 	
 	
