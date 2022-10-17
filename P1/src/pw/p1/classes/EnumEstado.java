d7055aff0a64f43794c45b249dd2fad06f0d8454
i92tacod <71820298+i92tacod@users.noreply.github.com>
2022-10-17 11:51:33 +0200

    Create EnumEstado.java

---
+++ P1/src/pw/p1/classes/EnumEstado.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * Enum Estado
+ * 
+ * @author David Olmo Cejudo
+ * @author Francisco Moreno Cano
+ * @author Ángel Quintero Montes
+ * @author Diego Tabas Costa
+ *
+ */
+
+public enum Estado {
+		DISPONIBLE, RESERVADO, MANTENIMIENTO;
+	}
+	
