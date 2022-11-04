ec817d267d3e9ce2fc893e61e7b72f12e712f342
david <david@DavidPC>
2022-11-04 13:42:32 +0100

    Creacion Ficheros de propiedades config.properties y sql.properties

---
+++ P2/src/pw/p2/display/ConfiguracionPropiedades.java
@@ -0,0 +1,42 @@
+package pw.p2.display;
+
+import pw.p2.business.*;
+import java.util.Properties;
+import java.io.*;
+import java.util.*;
+
+public class ConfiguracionPropiedades 
+
+{
+	public static void propiedadesConfiguracion() {
+		
+		Properties prop = new Properties();
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(new File("config.properties.txt")));
+			prop.load(reader);
+			
+			
+			String url = prop.getProperty("url");
+			String user = prop.getProperty("user");
+			String password = prop.getProperty("password");
+			String name = prop.getProperty("name");
+			
+			System.out.println("PROPIEDADES CONFIGURACION--------------------------------------");
+			System.out.println("URL="+url);
+			System.out.println("User="+user);
+			System.out.println("Password"+password);
+			System.out.println("Name="+name);
+			
+			System.out.println("-------------------------------------------------");
+			
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+	}
+}
