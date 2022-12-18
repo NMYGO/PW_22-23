78b34717c5c6cc6dfa37a06a254df8651f6d714a
david <david@DavidPC>
2022-12-18 19:51:12 +0100

    Actualizacion ModifyReservation

--- P3/src/main/java/pw/p3/servlet/client/ModifyReservation.java
+++ P3/src/main/java/pw/p3/servlet/client/ModifyReservation.java
@@ -34,11 +34,86 @@ public class ModifyReservation extends HttpServlet {
 				request.setAttribute("reservasAdultas", reservasAdultas);
 				request.setAttribute("reservasFamiliares", reservasFamiliares);
 				
+				String string_duracion = request.getParameter("duracion");
+				String string_fecha = request.getParameter("fecha");
+				String string_dificultad = request.getParameter("dificultad");
+				String string_ninos = "0";
+				String string_adultos = "0";
 				
+				for (int i = 0; i < reservasInfantiles.size(); i++) {
+					if(reservasInfantiles.get(i).getIdReserva() == Integer.parseInt(id_string)) 
+					{
+						reservaBean.setNinos(reservasInfantiles.get(i).getParticipantes());
+						reservaBean.setDuracion(reservasInfantiles.get(i).getDur());
+						//reservaBean.setDificultad(reservasInfantiles.get(i).getTipo());
+						reservaBean.setFecha(reservasInfantiles.get(i).getFecha());
+					}
+				}
 				
 				for (int i = 0; i < reservasAdultas.size(); i++) {
-					if 
+					if(reservasAdultas.get(i).getIdReserva() == Integer.parseInt(id_string)) 
+					{
+						reservaBean.setAdultos(reservasAdultas.get(i).getParticipantes());
+						reservaBean.setDuracion(reservasAdultas.get(i).getDur());
+						//reservaBean.setDificultad(reservasInfantiles.get(i).getTipo());
+						reservaBean.setFecha(reservasAdultas.get(i).getFecha());
+					}
 				}
+				
+				for (int i = 0; i < reservasFamiliares.size(); i++) {
+					if(reservasFamiliares.get(i).getIdReserva() == Integer.parseInt(id_string)) 
+					{
+						reservaBean.setAdultos(reservasFamiliares.get(i).getadultos());
+						reservaBean.setNinos(reservasFamiliares.get(i).getNinos());
+						reservaBean.setDuracion(reservasFamiliares.get(i).getDur());
+						//reservaBean.setDificultad(reservasInfantiles.get(i).getTipo());
+						reservaBean.setFecha(reservasFamiliares.get(i).getFecha());
+					}
+				}
+				
+				if (string_duracion != null)
+				{
+					reservaBean.setDuracion(Integer.parseInt(string_duracion));
+				}
+				
+				if (string_fecha != null)
+				{
+					reservaBean.setFecha(LocalDate.parse(string_fecha));
+				}
+				
+				if(string_dificultad != null)
+				{
+					reservaBean.setDificultad(string_dificultad);
+				}
+				
+				if (string_dificultad == "INFANTIL")
+				{
+					reservaBean.setAdultos(0);
+					if(string_ninos != null) {
+						reservaBean.setNinos(Integer.parseInt(string_ninos));
+					}
+				}
+				
+				if (string_dificultad == "ADULTO")
+				{
+					reservaBean.setNinos(0);
+					if(string_adultos != null) 
+					{
+						reservaBean.setAdultos(Integer.parseInt(string_adultos));
+					}
+				}
+				
+				if (string_dificultad == "FAMILIAR")
+				{
+					if(string_adultos != null) 
+					{
+						reservaBean.setAdultos(Integer.parseInt(string_adultos));
+					}
+					if(string_ninos != null) {
+						reservaBean.setNinos(Integer.parseInt(string_ninos));
+					}
+				}
+				
 				
 				if (id_string != null) {
 					Integer id = Integer.parseInt(id_string);
@@ -46,18 +121,18 @@ public class ModifyReservation extends HttpServlet {
 					if(reservationDAO.cancelReservaCliente(id) == 0) {
 						response.setContentType("text/html");
 						PrintWriter out = response.getWriter();
-						out.println("Error. Reserva no cancelada");
-						RequestDispatcher error = request.getRequestDispatcher("/mvc/view/client/CancelReservationView.jsp");
+						out.println("Error. Reserva no modificada");
+						RequestDispatcher error = request.getRequestDispatcher("/mvc/view/client/ModifyReservationView.jsp");
 						error.include(request, response);
 					} else {
 						response.setContentType("text/html");
 						PrintWriter out = response.getWriter();
-						out.println("CancelReservation");
+						out.println("ModifyReservation");
 						RequestDispatcher correcto = request.getRequestDispatcher("index.jsp");
 						correcto.include(request, response);
 					}
 				} else {
-					RequestDispatcher vista = request.getRequestDispatcher("/mvc/view/client/CancelReservationView.jsp");
+					RequestDispatcher vista = request.getRequestDispatcher("/mvc/view/client/ModifyReservationView.jsp");
 					vista.forward(request, response);
 				}
 			} else {
